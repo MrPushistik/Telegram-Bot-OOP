444ea36e2ee611343de4b91624aedb6523ca7715
MrPushistik <345w2011@mail.ru>
2022-10-30 19:20:45 +0400

    remove some imports

--- Bot/src/main/java/BotsPack/Bot.java
+++ Bot/src/main/java/BotsPack/Bot.java
@@ -5,8 +5,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jsoup.Jsoup;
